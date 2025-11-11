0f83cc6cae03332630cb8f4aedeac2005166ee2d
gomezjuanangel9-source <gomezjuanangel9@gmail.com>
2025-11-11 19:01:03 +0000

    cambios en el mundial

---
+++ predicción mundial.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const boton = document.getElementById("botonAbajo");
+
+  boton.addEventListener("click", function() {
+    window.location.href = "mundialypredicciones.html";
+  });
+});
+const titulos = [ "Alemania","España", "Francia","Inglaterra", "Países Bajos", "Portugal", 
+  "Croacia", "Serbia", "Suiza", "Dinamarca", "Polonia", "Bélgica", "Gales", "Argentina", "Brasil", "Uruguay", 
+  "Ecuador", "Senegal", "Marruecos", "Túnez", "Camerún", "Ghana", "Catar", "Arabia Saudita", "Japón", "Corea del Sur", "Irán", 
+  "Australia","México", "Estados Unidos", "Canadá", "Costa Rica"
+];
